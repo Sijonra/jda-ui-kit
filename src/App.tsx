23162f8ad2c79a96214b929e4a6fcdbc997ca725
Ilya Davydenko <ilya.davydenko@joy-dev.com>
2023-10-18 16:47:34 +0300

    feat[JDA-0305]: card layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.scss";
-
+import Card from "./components/Card/Card";
 import ButtonPage from "./pages/ButtonPage/ButtonPage";
 import plus from "./assets/svg/plus.svg";
 console.log(plus);
@@ -7,6 +7,26 @@ function App() {
   return (
     <>
       <ButtonPage />
+      <Card
+        elevation={0}
+        title="Card / Elevation 0"
+        subtitle="Floating Cards"
+      />
+      <Card
+        elevation={1}
+        title="Card / Elevation 1"
+        subtitle="Floating Cards"
+      />
+      <Card
+        elevation={2}
+        title="Card / Elevation 2"
+        subtitle="Floating Cards"
+      />
+      <Card
+        elevation={3}
+        title="Card / Elevation 3"
+        subtitle="Floating Cards"
+      />
     </>
   );
 }
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -3,17 +3,23 @@ import style from "./Card.module.scss";
 
 interface CardProps {
   elevation: number;
-  title: string;
-  text: string;
+  title?: string;
+  subtitle?: string;
+  children?: React.ReactNode;
 }
 
-const Card: FC<CardProps> = ({ elevation, title, text }) => {
-  console.log(elevation);
+const Card: FC<CardProps> = ({ elevation, title, subtitle, children }) => {
+  console.log("card_" + elevation.toString());
   return (
-    <div className={style.card}>
-      <h3 className={style["card__title"]}>{title}</h3>
-      <p className={style["card__text"]}>{text}</p>
-    </div>
+    <>
+      <div
+        className={style["card"] + " " + style["card_" + elevation.toString()]}
+      >
+        <h3 className={style["card__title"]}>{title}</h3>
+        <p className={style["card__subtitle"]}>{subtitle}</p>
+        <div className={style["card__content"]}>{children}</div>
+      </div>
+    </>
   );
 };
 
