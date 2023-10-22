22486cf47b71a2daa2a594cacd5871022de5b5da
Ilya Davydenko <ilya.davydenko@joy-dev.com>
2023-10-22 19:18:47 +0300

    feat[JDA-0303]: added Paragraph component

--- src/components/Paragraph/Paragraph.tsx
+++ src/components/Paragraph/Paragraph.tsx
@@ -1,5 +1,20 @@
-const Paragraph = () => {
-  return <>Paragraph</>;
-};
+import style from "./Paragraph.module.scss";
+import { FC } from "react";
+
+interface ParagraphProps {
+    className?: string;
+    type: string;
+    children?: React.ReactNode;
+}
+
+const Paragraph:FC<ParagraphProps> = ({className, type, children}) =>{
+    return(
+        <div className={style["paragraph"] + " " + className}>
+            <span className={style["paragraph__item"]  + " " + style["paragraph__item_" + type]}>
+                {children}
+            </span>
+        </div>
+    )
+}
 
 export default Paragraph;
--- src/pages/ParagraphPage/ParagraphPage.tsx
+++ src/pages/ParagraphPage/ParagraphPage.tsx
@@ -1,10 +1,32 @@
 import Menu from "../../components/Menu/Menu";
+import Paragraph from "../../components/Paragraph/Paragraph";
+
+import style from "./ParagraphPage.module.scss";
 
 const ParagraphPage = () => {
   return (
-    <>
-      <Menu /> ParagraphPage
-    </>
+    <div className={style["paragraph-page"]}> 
+      <Menu />
+
+      <section>        
+        <Paragraph type="small">
+          Used for multiline pieces of content. Lorem ipsum dolor sit amet, ex lucilius hendrerit vim, tempor scaevola iudicabit ei ius, te eum illud impetus antiopam. Eu wisi commune volutpat pro, usu at alii magna aperiam.
+        </Paragraph>
+      </section>
+
+      <section>        
+        <Paragraph type="medium">
+          Used for multiline pieces of content. Lorem ipsum dolor sit amet, ex lucilius hendrerit vim, tempor scaevola iudicabit ei ius, te eum illud impetus antiopam. Eu wisi commune volutpat pro, usu at alii magna aperiam.
+        </Paragraph>
+      </section>
+
+      <section>        
+        <Paragraph type="large">
+          Used for multiline pieces of content. Lorem ipsum dolor sit amet, ex lucilius hendrerit vim, tempor scaevola iudicabit ei ius, te eum illud impetus antiopam. Eu wisi commune volutpat pro, usu at alii magna aperiam.
+        </Paragraph>
+      </section>
+
+    </div>
   );
 };
 
