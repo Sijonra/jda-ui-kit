8f7c5a93538b7d64e850d674e13fcfbecea27fc0
Ilya Davydenko <ilya.davydenko@joy-dev.com>
2023-10-23 23:00:07 +0300

    feat: added deploy script

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/vite-react-router/",
     element: <App />,
   },
   {
